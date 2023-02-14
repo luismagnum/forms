1c2a70be6ec73c0507f3abecc0a7fa144ad2eacb
luismagnum <luismagnum1@gmail.com>
2023-02-13 23:08:28 -0300

    update emailjs

--- src/componentes/Formulario.js
+++ src/componentes/Formulario.js
@@ -1,24 +1,25 @@
-import React from 'react';
 import { useForm } from 'react-hook-form';
 import emailjs from '@emailjs/browser';
 
 const Formulario = ()=>{
-  const sendEmail = (event) =>{
-  event.preventDefault();
-   emailjs.sendForm('service_224j5hy','template_i4cdhbo',event.target,'V77rTj72eKT_6KpPi')
+  const sendEmail = (e) =>{
+   e.preventDefault();
+   emailjs.sendForm('service_224j5hy','template_i4cdhbo',e.target,'V77rTj72eKT_6KpPi')
+   .then(response=>console.log(response))
+   .catch(errors=>console.log(errors))
   }
   
-  const { register, formState:{ errors },handleSubmit } = useForm();
+  const { register, formState:{ errors } } = useForm();
     return(
     <div className='h-screen flex items-center justify-center '>
      <div className='bg-gray-200 max-h-full w-96 max-w-screen-sm mt-3 mb-3 border-2 rounded-3xl pt-5 px-5 py-6 resize:none shadow-2xl 
       shadow-black border-x-4'>
        <h2 className='text-center bg-blue-600 p-3 text-3xl font-bold rounded-tl-lg 
         rounded-tr-lg '>form</h2>
-       <form onSubmit={handleSubmit(sendEmail)}>
+       <form onSubmit={sendEmail}>
          <div className='mt-8'>
              <label className='text-lg font-bold'>Name</label>
-             <input className='w-full border-2 border-gray-500 rounded-xl p-2px mt-2px bg-transparent' type="text" 
+             <input className='w-full border-2 border-gray-500 rounded-xl p-2px mt-2px bg-transparent' type="text" name='user-name'
               {...register('nombre',{ required:true, maxLength:20 })} placeholder='max 20 characters'/>
              {errors.nombre?.type === 'required' && <p>the name field is required
               </p>}
@@ -34,7 +35,7 @@ const Formulario = ()=>{
          <div>
              <label className='text-lg font-bold'>email</label>
              <input
-              className='w-full border-2 border-gray-500 rounded-xl p-1 mt-1 bg- transparent'type='text' 
+              className='w-full border-2 border-gray-500 rounded-xl p-1 mt-1 bg- transparent'type='text' name='user-email'
               {...register('email',{ 
               pattern: /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/ })} placeholder='luismagnum1@gmail.com'/>
              {errors.email?.type === 'pattern' && <p>the email format is incorrect</p>}
